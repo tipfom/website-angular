d02990c9aed90774386aa52d02318fe49352f6bc
Tim <tipfom@hotmail.de>
2020-02-11 09:59:22 +0100

    added navigation bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LocationComponentComponent } from './location-component/location-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    NavigationBarComponent,
+    LocationComponentComponent
   ],
   imports: [
     BrowserModule,
