c922549e2c2a56399db0bdf40d3c7d8518fd2b94
Tim <tipfom@hotmail.de>
2020-02-22 15:35:00 +0100

    improved navigation bar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -12,4 +12,11 @@ export class NavigationBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  hrefClick(loc : string) : void{
+    location.href="#" + loc;
+  }
+
+  gotoPaypal() : void{
+    window.open("https://paypal.me/timpokart", "_blank");
+  }
 }
