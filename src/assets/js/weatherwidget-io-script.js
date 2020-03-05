cb1140ee694dc403b62ac6ba40121821d2822bfd
Tim <tipfom@hotmail.de>
2020-03-05 23:43:34 +0100

    fixed weather not loading

--- src/assets/js/weatherwidget-io-script.js
+++ src/assets/js/weatherwidget-io-script.js
@@ -1,3 +1,11 @@
-function weatherwidget_io_invoke(){
-    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+function weatherwidget_io_invoke() {
+    !function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        oldjs = d.getElementById(id);
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://weatherwidget.io/js/widget.min.js';
+        fjs.parentNode.insertBefore(js, fjs);
+        if (oldjs != null) oldjs.remove();
+    }(document, 'script', 'weatherwidget-io-js');
 }
