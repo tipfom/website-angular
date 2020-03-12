eb27f737e3211f156b804eeb4f1674f37c2edd39
Tim <tipfom@hotmail.de>
2020-03-12 21:16:18 +0100

    added smooth scrolling

--- src/app/pages/home/content-aboutme/content-aboutme.component.ts
+++ src/app/pages/home/content-aboutme/content-aboutme.component.ts
@@ -4,12 +4,6 @@ import { Observable } from 'rxjs';
 
 declare function weatherwidget_io_invoke(): any;
 
-const ADDITIONAL_MILESTONES: MilestoneEntry[] = [
-  { name: 'Wissenschaftliche Hilfskraft am Fraunhofer IWS', timespan: 'März - August 2019' },
-  { name: 'Teil der deutschen Mannschaft zur Internationalen Physikolympiade 2018', timespan: '21. - 29. Juli 2018' },
-  { name: 'Abitur am Max-Steenbeck-Gymnasium', timespan: '22.06.2018' },
-  { name: 'Landessieger bei der Brandenburger Physikolympiade', timespan: '2014, 2015, 2016, 2018' },
-];
 
 @Component({
   selector: 'app-content-aboutme',
@@ -17,20 +11,48 @@ const ADDITIONAL_MILESTONES: MilestoneEntry[] = [
   styleUrls: ['./content-aboutme.component.sass']
 })
 export class ContentAboutmeComponent implements OnInit {
-  index = 1;
-  selectedMilestone = new Observable<MilestoneEntry>(observer => {
-    observer.next(ADDITIONAL_MILESTONES[this.index]);
-    setInterval(()=>{
-      this.index++;
-      if(this.index >= ADDITIONAL_MILESTONES.length) this.index = 0;
-      observer.next(ADDITIONAL_MILESTONES[this.index]);
-    },6000);
-  });
+  index = 0;
 
-  constructor() { 
+  additionalMilestones: MilestoneEntry[] = [
+    { name: 'Wissenschaftliche Hilfskraft am Fraunhofer IWS', timespan: 'März - August 2019' },
+    { name: 'Teil der deutschen Mannschaft zur Internationalen Physikolympiade 2018', timespan: '21. - 29. Juli 2018' },
+    { name: 'Abitur am Max-Steenbeck-Gymnasium', timespan: '22.06.2018' },
+    { name: 'Landessieger bei der Brandenburger Physikolympiade', timespan: '2014, 2015, 2016, 2018' },
+  ];
+  headerHeight: string;
+
+  constructor() {
+    setInterval(() => {
+      this.index++;
+      if (this.index == this.additionalMilestones.length) this.index = 0;
+      this.scrollToIndex();
+    }, 10000);
   }
 
   ngOnInit(): void {
     weatherwidget_io_invoke();
+  }
+
+  scrollToIndex(): void {
+    let x = document.getElementById("shuffleeventsdiv");
+    let start_scroll = x.scrollLeft;
+    let target_scroll = (<HTMLDivElement>x.getElementsByClassName("shuffleentrydiv")[this.index]).offsetLeft - x.offsetLeft;
+    let animation_time = 1000;
+    let animation_speed = 5;
+    let progress = 0;
+    let timer = setInterval(() => {
+      progress += animation_speed / animation_time;
+      if (progress < 1) {
+        let scroll = start_scroll + this.scrollProgress(progress) * (target_scroll - start_scroll);
+        x.scrollTo(scroll, 0);
+      } else {
+        x.scrollTo(target_scroll, 0);
+        clearInterval(timer);
+      }
+    }, animation_speed);
+  }
+
+  scrollProgress(progress: number): number {
+    return 0.5*(1 - Math.cos(progress * Math.PI));
   }
 }
