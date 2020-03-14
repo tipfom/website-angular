63b9cb09ee0dae805461b6ac185d277e4c42302f
Tim <tipfom@hotmail.de>
2020-03-14 10:20:45 +0100

    added translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule, SecurityContext } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MarkdownModule } from 'ngx-markdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './pages/home/landing-page/landing-page.component';
-import { HomeNavigationBarComponent } from './pages/home/home-navigation-bar/home-navigation-bar.component';
-import { ContentContactComponent } from './pages/home/content-contact/content-contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContentAboutmeComponent } from './pages/home/content-aboutme/content-aboutme.component';
-import { AboutmeComponent } from './pages/aboutme/aboutme.component';
-import { HomeComponent } from './pages/home/home.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { AboutmeComponent } from './pages/aboutme/aboutme.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { CmsComponent } from './pages/admin/cms/cms.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { ArticleComponent } from './pages/article/article.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { ContentAboutmeComponent } from './pages/home/content-aboutme/content-aboutme.component';
+import { ContentContactComponent } from './pages/home/content-contact/content-contact.component';
+import { ContentSpotlightComponent } from './pages/home/content-spotlight/content-spotlight.component';
+import { HomeNavigationBarComponent } from './pages/home/home-navigation-bar/home-navigation-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LandingPageComponent } from './pages/home/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ResourceComponent } from './pages/resource/resource.component';
-import { ArticleComponent } from './pages/article/article.component'; 
-import { MarkdownModule } from 'ngx-markdown';
-import { ContentSpotlightComponent } from './pages/home/content-spotlight/content-spotlight.component';
-import { ArticlesComponent } from './pages/articles/articles.component';
 
 @NgModule({
   declarations: [
@@ -49,9 +50,21 @@ import { ArticlesComponent } from './pages/articles/articles.component';
     ReactiveFormsModule,
     MarkdownModule.forRoot({
       sanitize: SecurityContext.NONE
+    }),
+    TranslateModule.forRoot({
+      loader:{
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
     })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
