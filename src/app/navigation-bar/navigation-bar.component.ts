5a5330b3aa486e08b6a5f830a9e46632fa48930f
Tim <tipfom@hotmail.de>
2020-03-15 11:16:07 +0100

    language popup in navbar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LanguagePopupComponent } from '../popups/language-popup/language-popup.component';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
+  @ViewChild(LanguagePopupComponent) languagePopup;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -14,5 +17,9 @@ export class NavigationBarComponent implements OnInit {
 
   gotoContact(): void {
     location.href = "#contact";
+  }
+
+  showLanguagePopup(){
+    this.languagePopup.show();
   }
 }
