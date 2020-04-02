a3134f03f651bda7eb6ab92984480f20faba20e6
Tim <tipfom@hotmail.de>
2020-04-02 20:17:35 +0200

    added sources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, HostListener } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AnchorService } from './services/anchor.service';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationEnd, Router, NavigationStart } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 
 declare let gtag: Function;
@@ -20,6 +21,7 @@ export class AppComponent {
     else translateService.setDefaultLang("en");
 
     this.router.events.subscribe(event => {
+      if (!environment.production && event instanceof NavigationStart) console.log(event);
       if (event instanceof NavigationEnd) {
         gtag('config', 'UA-162304129-1', { 'page_path': event.urlAfterRedirects });
       }
