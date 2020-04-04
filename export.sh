9d9f23a0e735505ee6ddeb6b2666b4160e6d476a
Tim <tipfom@hotmail.de>
2020-04-04 13:22:09 +0200

    annotated export.sh

--- export.sh
+++ export.sh
@@ -1,4 +1,8 @@
+echo 'pulling from git'
+git pull
+echo 'building webapp'
 ng build --prod --aot
+echo 'creating docker container'
 docker build --tag=website-nginx .
 docker save website-nginx -o website-nginx.tar
 chown tim website-nginx.tar
